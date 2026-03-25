40bdc0fdabe50dbad6ddc6cc3b4266437dc67310
shashank chourasia01 <shashankchourasia0829@gmail.com>
2026-03-26 00:18:06 +0530

    qf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
+  plugins: [ viteCompression(),tailwindcss(),react()],
 })
